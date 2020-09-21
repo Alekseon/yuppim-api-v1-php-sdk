3f6823e07c2afc44f27ede14f8642aac95569353
marcinfr <m.frymark@alekseon.com>
2020-09-21 15:23:46 +0200

    composer.json

---
+++ src/Test.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright © Alekseon sp. z o.o.
+ * http://www.alekseon.com/
+ */
+
+//use Yuppim;
+
+$y = new Yuppim();
+var_dump($y->getBaseUrl());
---
+++ src/Yuppim.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Copyright © Alekseon sp. z o.o.
+ * http://www.alekseon.com/
+ */
+namespace YuppimApi;
+
+class Yuppim
+{
+    /**
+     * @return string
+     */
+    public function getBaseUrl()
+    {
+        return 'test';
+    }
+}
